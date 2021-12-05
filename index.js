ac948767162c30011a3a13678914e64911fb3ca4
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2021-12-05 15:58:14 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let random = Math.floor(Math.random()* 10) - 1;
 
 let num3 = 25;
 let num4 = 7;
-let mod = 25 % 7;
+let mod = num3 % num4;
 
 
 let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
