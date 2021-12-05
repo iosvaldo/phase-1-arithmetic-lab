7a55d56d06f12facc66009e965b1a999350a9f50
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2021-12-05 15:37:35 -0500

    Completed Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+
+let num1 = 31;
+let num2 = 3;
+let multiply = 31 * 2;
+
+let random = Math.floor(Math.random()* 10) - 1;
+
+let num3 = 25;
+let num4 = 7;
+let mod = 25 % 7;
+
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
