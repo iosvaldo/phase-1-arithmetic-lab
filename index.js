b939db7838706bc3e4541fbc0e5674b2993d0c9a
osvaldo <iosvaldo.app@gmail.com>
2022-08-17 16:45:26 -0400

    commit tree e66e484b6601c7221ec82d04c6600152aab4126a

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 
 // Write your code here
 
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-let num1 = 31;
-let num2 = 2;
-let multiply = num1 * num2;
+const random = Math.floor(Math.random() > 0)
 
-let random = Math.floor(Math.random()* 10) - 1;
+const num3 = 16
+const num4 = 4
+const mod = num3 / num4;
 
-let num3 = 25;
-let num4 = 7;
-let mod = num3 % num4;
-
-
-let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+const max = Math.max(1,2,3,4,5,20);
